76a11cdafed6586260437c671529a95a0e7a34cd
shazxrin <sadmeowkins@outlook.com>
2020-06-04 13:41:18 +0800

    Configuration header file

---
+++ src/Config.hpp
@@ -0,0 +1,11 @@
+#pragma once
+
+#include <cstdint>
+
+struct Config
+{
+	static constexpr uint16_t TARGET_FPS = 60;
+	static constexpr uint16_t WINDOW_WIDTH = 800;
+	static constexpr uint16_t WINDOW_HEIGHT = 600;
+	static constexpr uint16_t FOV = 60;
+};
