6feadfa3c21a3b06e7dd37a3fdfea041c96e5f6d
shazxrin <sadmeowkins@outlook.com>
2020-07-15 19:06:16 +0800

    Lower OpenGL version

--- src/Main.cpp
+++ src/Main.cpp
@@ -20,8 +20,8 @@ int main()
 {
 	// Intialize GLFW.
 	glfwInit();
-	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 4);
-	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 6);
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
 	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
 
 	// Initialize OpenGL window.
